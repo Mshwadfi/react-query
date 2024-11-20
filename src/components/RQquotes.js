2afcbf33aa51848acb20008c2fff8d99eab3f023
Mshwadfi <mohamedalshwadfy24@gmail.com>
2024-11-20 07:49:48 +0200

    onSuccess & onError

--- src/components/RQquotes.js
+++ src/components/RQquotes.js
@@ -4,15 +4,29 @@ import Shimmer from './Shimmer'
 
 const RQquotes = () => {
 
+  const onSuccess = ()=>{
+    console.log('perform Side Effect after data fetched correctly');
+  }
+  const onError = ()=>{
+    console.log('perform Side Effect after data fetching fails');
+  }
+
   const {isLoading, data, isError, error, refetch} = useQuery('Quotes', async()=>{
-      const res = await fetch('http://localhost:4000/quotes')
+      const res = await fetch('http://localhost:4000/quotesh')
       return res.json();
   },
   {
-    // enabled: false,
+    // staleTime: 3000, // Data remains fresh for 3000ms (3 seconds) before becoming stale
+    // refetchOnMount: true, // Refetches data when the component mounts "default"
+    // refetchOnWindowFocus: true, // Refetches data when the window regains focus "default"
+    // refetchInterval: 5000, // Automatically refetches data every 5000ms (5 seconds) - works only if the window is focuses
+    // refetchIntervalInBackground: 2000, // Refetches data every 2000ms (2 seconds) even when the window is not focused
+    // enabled: false, // Disables automatic fetching; must call refetch manually to fetch data
+    onSuccess,
+    onError,
   }
 )
-  console.log(error)
+  // console.log(error)
   if(isLoading) return <Shimmer />
   if(isError) return <h2>{error.message}</h2>
   return (
